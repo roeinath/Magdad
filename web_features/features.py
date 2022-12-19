c39d286e4e656ace4d4df57541013d878574e510
רואי נתנזון <T9079575@talpiot.huji.ac.il>
2022-12-19 15:33:19 +0200

    WIP - save to add permissions update

--- web_features/features.py
+++ web_features/features.py
@@ -8,11 +8,9 @@ from web_features.cleaning_duties.cleaning_category import CleaningCategory
 from web_features.logistic_events.logistic_category import LogisticEventsCategory
 from web_features.Elements.elements_category import ElementsCategory
 from web_features.tech_miun.miun_category import MiunCategory
-from web_features.tech_miun_temp.miun_category import MiunCategoryTemp
 
 categories = [
     ElementsCategory(),
     TalpiXCategory(),
     MiunCategory(),
-    MiunCategoryTemp()
 ]
--- web_features/tech_miun/miun_category.py
+++ web_features/tech_miun/miun_category.py
@@ -4,6 +4,8 @@ from web_features.tech_miun.assessments.estimator_page import EstimatorPage
 from web_features.tech_miun.handle_estimators import HandleEstimators
 from web_features.tech_miun.master_page import MasterPage
 from web_features.tech_miun.assessments.sagab_miun_page import SagabPage
+from web_features.tech_miun_temp.assessments.zoer_page import ZoerPage
+from web_features.tech_miun_temp.assessments.zoer_dummy_page import DummyZoerPage
 from web_framework.server_side.infastructure.category import Category
 
 
@@ -15,11 +17,13 @@ class MiunCategory(Category):
             "saga_miun_page": SagabPage,
             "behavioral_diagnostician_miun_page": BehavioralDiagnosticianPage,
             "handle_estimators": HandleEstimators,
-            "master_page": MasterPage
+            "master_page": MasterPage,
+            "zoer_page": ZoerPage,
+            "dummy_zoer_page": DummyZoerPage,
         })
 
     def get_title(self) -> str:
-        return "להתעלם"
+        return "מיון והערכה"
 
     def is_authorized(self, user):
         return permissions.is_user_miun(user)
