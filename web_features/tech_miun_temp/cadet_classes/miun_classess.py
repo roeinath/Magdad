5a674fb9ebce7d2e8bf351f01cdff76d99f18b15
Shmunik <matan.shmunik.44@gmail.com>
2022-12-29 19:30:53 +0200

    imported Pandas added load_data_from_csv_or_excel added self.is_real_data formatted the file

--- web_features/tech_miun_temp/cadet_classes/miun_classess.py
+++ web_features/tech_miun_temp/cadet_classes/miun_classess.py
@@ -19,6 +19,8 @@ from APIs.TalpiotAPIs.AssessmentAPI.tsyunomat.GetDataFromDB import *
 from os import chdir
 from os.path import abspath, dirname
 from web_features.Elements.personal_page.permissions import is_user_captain
+import pandas as pd
+
 SIZE_EXTRA_SMALL = 'xs'
 SIZE_SMALL = 'sm'
 SIZE_MEDIUM = 'md'
@@ -31,25 +33,39 @@ class CadetMiunGrades:
         self.user: User = user
         self.year: int = year
         self.cadet: str = cadet
+        self.is_real_data = is_real_data
 
         self.grades_layout: GridPanel = None
         self.popup: PopUp = None
 
     def get_ui(self):
         self.grades_layout = GridPanel(3, 1, bordered=False)
-        self.load_user_from_drive()
-        self.grades_layout.add_component(self.get_table(), 0, 0)
+        self.load_user_from_drive(self.user)
+        self.grades_layout.add_component(self.get_grades_table(), 0, 0)
         return self.grades_layout
 
-    def load_user_from_drive(user_name):
-        #TODO: match mahzor to year of miyun and check 4 years back
+    @staticmethod
+    def load_data_from_csv_or_excel(path):
+        """
+        creates data table from file in path
+        :param path: the path
+        :return: DataFrame with the data
+        """
+        try:
+            df = pd.read_csv(path, header=0, skip_blank_lines=True,
+                             skipinitialspace=True, encoding='latin-1')
+        finally:
+            df = pd.read_excel(path, header=0)
+
+    def load_user_from_drive(self, user_name):
+        # TODO: match mahzor to year of miyun and check 4 years back
         #      for all excels in which the cadet appear
         #      save all the data in a list of lists
         #      will be later shown in accordion
-        self.table_titles = ['a','b','c','d','e']
-        self.table_data = [[1,1,1], [2,2,2], [3,3,3], [4,4,4], [5,5,5]]
+        self.table_titles = ['a', 'b', 'c', 'd', 'e']
+        self.table_data = [[1, 1, 1], [2, 2, 2], [3, 3, 3], [4, 4, 4], [5, 5, 5]]
 
-    def get_table(self):
+    def get_grades_table(self):
         """
         pulls the grades and puts them in a table
         :return: GridPanel with grades
@@ -60,14 +76,13 @@ class CadetMiunGrades:
 
         grades_gp = GridPanel(len(self.table_data[0]) + 4, len(self.table_titles))
 
-
         for i, title in enumerate(self.table_titles):
             grades_gp.add_component(Label(title, size=SIZE_LARGE, fg_color="white"), row=0, column=i,
                                     bg_color=COLOR_PRIMARY_DARK)
             for j, data in enumerate(self.table_data[i]):
-                if(not data):
-                    grades_gp.add_component(Label(text="לא ידוע"), row=j+1, column=i)
+                if not data:
+                    grades_gp.add_component(Label(text="לא ידוע"), row=j + 1, column=i)
                 else:
-                    grades_gp.add_component(Label(text=str(data)), row=j+1, column=i)
+                    grades_gp.add_component(Label(text=str(data)), row=j + 1, column=i)
 
         return grades_gp
