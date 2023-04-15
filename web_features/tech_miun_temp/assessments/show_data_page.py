4acaef7513a7a6b016e7cbccb758e6689b9ba378
eladbiran <elad.biran.44@gmail.com>
2023-04-15 20:37:11 +0300

    הצלחה בדף הצגת מידע

--- web_features/tech_miun_temp/assessments/show_data_page.py
+++ web_features/tech_miun_temp/assessments/show_data_page.py
@@ -23,7 +23,7 @@ from web_framework.server_side.infastructure.components.button import Button
 from web_framework.server_side.infastructure.components.divider import Divider
 from web_framework.server_side.infastructure.constants import *
 from web_features.tech_miun_temp.cadet_classes.utils import Data
-from APIs.ExternalAPIs.MiunDrive.MiunDriveAPI import get_list_of_all_data_files, update_file
+from APIs.ExternalAPIs.MiunDrive.MiunDriveAPI import get_list_of_all_data_files, update_file , open_file
 
 class ShowDataPage(Page):
 
@@ -38,15 +38,53 @@ class ShowDataPage(Page):
     def is_authorized(user) -> bool:
         return permissions.is_estimator_miun(user)
 
-    def on_select_combo_box(self, selected_file):
-        self.current_file = self.current_file.get_child(selected_file)
+    def on_option_selected(self, value, field_name):
+        print('+++++++++++++++++++++++++++++++++')
+        selected_column = self.df[str(self.df.columns[int(field_name)])]
+        column_list = selected_column.to_list()
+        print(column_list)
+        print(value)
+        print('%%%%%%%%%%%%%%%%%%%%%%%%%%%5')
+        print(self.df[str(self.df.columns[int(field_name)])])
+        print('!!!!!!!!!!!!!!!!!!!!!!')
+        print(self.df.columns)
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        print(column_list[int(value)])
+        filtered_df = self.df[str(self.df.columns[int(field_name)])] == column_list[int(value)]
+        print(filtered_df)
+        print('@@@@@@@@@@@@@@@@@@@@@@@@')
+        print(str(filtered_df.iloc))
+        print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+        row_data = self.df.iloc[self.df.loc[filtered_df].index[0]]
+        print(row_data)
+        print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
+
+    def on_field_selected(self, field_name, field_val):
+        option_list = self.df[str(self.df.columns[int(field_name)])].apply(str).tolist()
+        self.sp.add_component(ComboBox(option_list, on_changed=lambda selected_value: self.on_option_selected(selected_value, field_name)))
+
+    def on_select_combo_box(self, selected_file, combobox_id):
+        self.current_file = self.files[combobox_id].get_child(selected_file)
         children = self.current_file.get_all_children()
+
+        # TODO: Remove comboboxes when uses upper combobox
+        ##################################################
+        for i in range(combobox_id + 1, len(self.file_combos)):
+            self.sp.delete_component(self.file_combos[i])
+        self.files = self.files[:combobox_id+1]
+        self.files.append(self.current_file)
+        self.file_combos = self.file_combos[:combobox_id+1]
+        ###################################################
         if(children):
             self.file_combos.append(ComboBox(children,
-                                            on_changed=lambda selected_file: self.on_select_combo_box(children[int(selected_file)])))
+                                            on_changed=lambda selected_file: self.on_select_combo_box(children[int(selected_file)],
+                                                                                                      combobox_id+1)))
             self.sp.add_component(self.file_combos[-1])
         else:
-            print("I Am Done")
+            update_file(self.current_file)
+            self.df = open_file(self.current_file)           #opens file as pandas dataframe
+            options_list=[str(i) for i in self.df.columns]
+            self.sp.add_component(ComboBox(options_list,on_changed=lambda selected_option: self.on_field_selected(selected_option,0)))
 
     def get_page_ui(self, user: User):
         self.user = user
@@ -54,11 +92,13 @@ class ShowDataPage(Page):
         self.file_combos = []
 
         root = get_list_of_all_data_files()
+        print(root)
         self.current_file = root
+        self.files = [root]
         children = root.get_all_children()
         if(children):
             self.file_combos.append(ComboBox(children,
-                                        on_changed=lambda selected_file: self.on_select_combo_box(children[int(selected_file)])))
+                                        on_changed=lambda selected_file: self.on_select_combo_box(children[int(selected_file)],0)))
         else:
             #print an error
             pass
