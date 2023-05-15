276119d03b372a9b74f876ee5a0fd54685f52bfe
eladbiran <elad.biran.44@gmail.com>
2023-05-15 23:53:03 +0300

    הצלחה בדף עיצוב חופשי- הצגת מידע על צוערים

--- web_features/tech_miun_temp/wix/custom_page.py
+++ web_features/tech_miun_temp/wix/custom_page.py
@@ -27,6 +27,7 @@ from web_framework.server_side.infastructure.constants import *
 from web_features.tech_miun_temp.cadet_classes.utils import Data
 from APIs.ExternalAPIs.MiunDrive.MiunDriveAPI import get_list_of_all_data_files, update_file, open_file, get_file_object
 from web_features.tech_miun_temp.wix.utils import fetch_fields_dict, ID_names, CUSTOM_PAGES_DIR
+from web_features.tech_miun_temp.wix import graph_functions,statistics_functions
 
 
 class CustomPage(Page):
@@ -43,37 +44,62 @@ class CustomPage(Page):
     def is_authorized(user) -> bool:
         return permissions.is_estimator_miun(user)
 
+    def fetch_field_data(self,field):
+        name_file = field.split(':')[0]
+        name_column = field.split(':')[1]
+        current_file=self.root
+        children = self.root.get_all_children()
+        current_file = get_file_object(current_file, name_file)
+        update_file(current_file)
+        self.df = open_file(self.current_file)
+        return name_column,self.df[name_column]
+
+
     def fetch_field_values(self,user,root,person_id,field):
-        print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
         print(self.person_id)
-        print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
         name_file=field.split(':')[0]
         name_column=field.split(':')[1]
         self.user = user
         self.file_combos = []
         self.current_file = root
         self.files = [root]
-        print('##################################')
         children = root.get_all_children()
         self.current_file = get_file_object(root,name_file)
-        print('##################################')
         update_file(self.current_file)
         self.df = open_file(self.current_file)  # opens file as pandas dataframe
         for id_name in ID_names:
             if(id_name in self.df):
                 print(self.df[self.df[id_name] == person_id])
                 value = self.df.loc[self.df[id_name] == person_id, name_column].iloc[0]
-                print('##################################')
                 print(value)
                 return value
         raise Exception('No ID Field')
 
     def return_id(self,number,user):
         self.person_id=int(number)
-        print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
-        print(self.person_id)
-        print(number)
         self.update_page(user)
+
+    def read_func_js(self,groups_dict,graph,graph_name,parameters_list):
+        parameters=[]
+        xy=['x','y']
+        param={'x':[],'y':[]}
+        type_func=parameters_list[0]
+        func_parameters=parameters_list[1]
+        for i in range(2):
+            if func_parameters[i][0]!='no_func':
+                name,data=self.fetch_field_data(func_parameters[i][1])
+                stat_para=(statistics_functions.STATISTICS_FUNCTIONS[func_parameters[i][0]](data))
+                param[xy[i]].append(name)
+                param[xy[i]].append(data)
+                param[xy[i]].append(stat_para)
+            else:
+                name,data=self.fetch_field_data(func_parameters[i][1])
+                param[xy[i]].append(name)
+                param[xy[i]].append(data)
+        action=graph_functions.GRAPH_FUNCTIONS[type_func]
+        action(graph, param['x'][1], param['y'][1], graph_name)
+
+
 
     def update_page(self,user):
         with open(os.path.join(CUSTOM_PAGES_DIR,self.selected_page_name),'r') as f:
@@ -99,7 +125,7 @@ class CustomPage(Page):
         #fetch_fields_dict(root: FileTree, json_dict: dict, candidate_id: int)
         with open(os.path.join(CUSTOM_PAGES_DIR, page_name),'r') as f:
             groups_dict = json.load(f)
-            root = get_list_of_all_data_files()
+            self.root = get_list_of_all_data_files()
             #print('Now\n',fetch_fields_dict(root, groups_dict, 12),'\nEND')
             group_names = []
             group_layouts = []
@@ -110,16 +136,42 @@ class CustomPage(Page):
                 index = 0
                 for field_name, field in fields_dict.items():
                     group_layout.add_component(Label(field_name, fg_color='White'), 0, index)
-                    real_value = self.fetch_field_values(self.user, root, self.person_id, field)
+                    real_value = self.fetch_field_values(self.user, self.root, self.person_id, field)
                     self.labels[field]=Label(real_value, fg_color='White')
-                    print('##########################')
                     group_layout.add_component(self.labels[field], 1, index)   #real_value_dict[field]
                     index += 1
                 group_layouts.append(group_layout)
 
             accordion = Accordion(group_layouts, group_names)
             self.custom_stack.add_component(accordion)
-            #self.sp.add_component(self.custom_stack)
+            '''
+            for i in json:
+                graph= ChartjsComponent()
+                group_layout.add_component(graph_i)
+            '''
+            horizontal_sp = StackPanel(orientation=HORIZONTAL)
+            vertical_sps = [StackPanel(), StackPanel(), StackPanel()]
+            vertical_graphs = []
+
+            ######################################################################
+            example_chart = ChartjsComponent(width='500px', height='450px')
+            example_chart.plot([1, 1, 1, 1], [0, 1, 2, 3])
+            example_chart.title("משתמשים באתר בכל יום", size=30)
+            ######################################################################
+            dic = groups_dict['Graphs']
+            num_graphs = 0
+            for graph_name, parameters_list in dic.items():
+                vertical_graphs.append(ChartjsComponent(width='500px', height='450px'))
+                vertical_sps[num_graphs % 3].add_component(vertical_graphs[-1])
+                self.read_func_js(vertical_graphs[-1], graph_name, parameters_list)
+                num_graphs += 1
+
+            for stack in vertical_sps:
+                horizontal_sp.add_component(stack)
+            #graph_accordion=Accordion([horizontal_sp],['גרפים'])
+            self.sp.add_component(Label(text='גרפים', bg_color= COLOR_PRIMARY_DARK))
+            self.sp.add_component(horizontal_sp)
+
 
     def get_page_ui(self, user: User):
         self.user = user
@@ -136,7 +188,7 @@ class CustomPage(Page):
             )
 
         self.sp.add_component(self.custom_stack)
-
+       
         return self.sp
 
    
--- web_features/tech_miun_temp/wix/graph_functions.py
+++ web_features/tech_miun_temp/wix/graph_functions.py
@@ -1,5 +1,33 @@
-GRAPH_FUNCTIONS = 
-{
+def plot(chartJsComponent, x, y, title):
+    chartJsComponent.plot(x, y)
+    chartJsComponent.title(title, size=30)
+
+
+def bar(chartJsComponent, x, y, title):
+    chartJsComponent.bar(x, y)
+    chartJsComponent.title(title, size=30)
+
+
+def radar(chartJsComponent, x, y, title):
+    chartJsComponent.radar(x, y)
+    chartJsComponent.title(title, size=30)
+
+
+def scatter(chartJsComponent, x, y, title):
+    chartJsComponent.scatter(x, y)
+    chartJsComponent.title(title, size=30)
+
+
+def pie(chartJsComponent, x, y, title):
+    chartJsComponent.pie(x, y)
+    chartJsComponent.title(title, size=30)
+
+
+def doughnut(chartJsComponent, x, y, title):
+    chartJsComponent.doughnut(x, y)
+    chartJsComponent.title(title, size=30)
+
+GRAPH_FUNCTIONS = {
     'plot': plot,
     'bar': bar,
     'radar': radar,
@@ -8,26 +36,3 @@ GRAPH_FUNCTIONS =
     'doughnut': doughnut,
 }
 
-def plot(chartJsComponent, x, y, title):
-        chartJsComponent.plot(x, y)
-        chartJsComponent.title(title, size=30)
-
-def bar(chartJsComponent, x, y, title):
-        chartJsComponent.bar(x, y)
-        chartJsComponent.title(title, size=30)
-
-def radar(chartJsComponent, x, y, title):
-        chartJsComponent.radar(x, y)
-        chartJsComponent.title(title, size=30)
-
-def scatter(chartJsComponent, x, y, title):
-        chartJsComponent.scatter(x, y)
-        chartJsComponent.title(title, size=30)
-
-def pie(chartJsComponent, x, y, title):
-        chartJsComponent.pie(x, y)
-        chartJsComponent.title(title, size=30)
-
-def doughnut(chartJsComponent, x, y, title):
-        chartJsComponent.doughnut(x, y)
-        chartJsComponent.title(title, size=30)
